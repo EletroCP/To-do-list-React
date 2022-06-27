aa41cf89b356ce0f109d38b2221164adeb1cc74a
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-27 20:27:06 -0300

    feat: Estilização mobile do header implementada

--- src/componets/HeaderApp.js
+++ src/componets/HeaderApp.js
@@ -6,8 +6,12 @@ import { MdOutlineAddTask } from 'react-icons/md'
 const HeaderApp = () => {
   return (
     <header className="App-header">
-      <input type='text' className='toDo'/>
-      <button className='insert'><MdOutlineAddTask/></button>
+      <input
+        type='text'
+        className='toDo'
+        placeholder='Nova tarefa'
+      />
+      <button className='insert'><MdOutlineAddTask className='iconInsert'/></button>
     </header>
   );
 };
