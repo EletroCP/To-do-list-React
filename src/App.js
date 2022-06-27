4c0d51a0454a61ea744033e40ffb9e0f69c3b96f
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-27 19:21:32 -0300

    feat: Alterado o title da pagina para "To-do-List"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  document.title = 'To-do-List';
   return (
     <div className="App">
       <header className="App-header">
