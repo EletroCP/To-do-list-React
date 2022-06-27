aeb3cb01952a0edd4696a0dc5ac7d43d0bc6e47f
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-27 20:11:08 -0300

    feat: Icone adicionado no botão de adicionar tarefa

--- src/componets/HeaderApp.js
+++ src/componets/HeaderApp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
 
+import { MdOutlineAddTask } from 'react-icons/md'
+
 const HeaderApp = () => {
   return (
     <header className="App-header">
       <input type='text' className='toDo'/>
-      <button className='insert'/>
+      <button className='insert'><MdOutlineAddTask/></button>
     </header>
   );
 };
